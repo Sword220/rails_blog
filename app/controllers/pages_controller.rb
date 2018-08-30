1372cdba2915498e8ace11970ebc5baf7ead7c22
Pete Sword <pete.sword@yahoo.com>
2018-08-30 11:27:25 -0600

    index and show render, pages controller built out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,46 @@
 class PagesController < ApplicationController
+  
   def index
+    @pages = Page.all
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
+
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @page = Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:title, :author, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pages
 end
